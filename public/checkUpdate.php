4c63bc0e0094c6f9bd57a2a84c582204fff4f091
okamuratakeru <takeokamura11@gmail.com>
2025-07-20 19:35:22 +0800

    タスクの編集機能の追加

---
+++ public/checkUpdate.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once __DIR__.'/../includes/db.php';
+require_once __DIR__.'/../includes/functions.php';
+require_login();
+verify_csrf();
+
+$id = trim($_POST['id'] ?? '');
+$isDone = trim($_POST['is_done'] ?? '');
+
+error_log(print_r($_POST, true));
+
+if ($id !== '') {
+    if ($isDone == 0) {
+      $isDone = 1;
+    }elseif($isDone == 1) {
+      $isDone = 0;
+    }
+    $pdo = db();
+    $stm = $pdo->prepare('UPDATE tasks SET is_done = :isDone, updated_at = CURRENT_TIMESTAMP WHERE id = :id AND user_id = :user_id');
+    $stm->execute([':id' => $id, ':isDone' => $isDone, ':user_id' => $_SESSION['user_id']]);
+}
+header('Location: index.php');         
--- public/update.php
+++ public/update.php
@@ -1,22 +1,16 @@
 <?php
-session_start();
 require_once __DIR__.'/../includes/db.php';
 require_once __DIR__.'/../includes/functions.php';
+init_session();
 require_login();
 verify_csrf();
 
 $id = trim($_POST['id'] ?? '');
-$isDone = trim($_POST['is_done'] ?? '');
-
-if ($id !== '') {
-    if ($isDone == 0) {
-      $isDone = 1;
-    }elseif($isDone == 1) {
-      $isDone = 0;
-    }
-    
-    $pdo = db();
-    $stm = $pdo->prepare('UPDATE tasks SET is_done = :isDone, updated_at = CURRENT_TIMESTAMP WHERE id = :id AND user_id = :user_id');
-    $stm->execute([':id' => $id, ':isDone' => $isDone, ':user_id' => $_SESSION['user_id']]);
+$title = trim($_POST['title'] ?? '');
+if ($id !== '' && $title !== '') {
+  $pdo = db();
+  $stm = $pdo->prepare('UPDATE tasks SET title = :title WHERE id = :id AND user_id = :user_id');
+  $stm->execute([':id' => $id, ':title' => $title, 'user_id' => $_SESSION['user_id']]);
 }
-header('Location: index.php');         
+header('Location: index.php');
+?>
